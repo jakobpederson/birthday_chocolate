55199c7ab515ea05625b280553b4db751fe20679
jakobpederson <jakob.pederson+github@gmail.com>
2017-05-23 08:30:06 -0500

    Refactor: improve solve method

--- test_birthday_chocolate.py
+++ test_birthday_chocolate.py
@@ -3,17 +3,14 @@ import unittest
 
 VALUE_ONE = [5, [1, 2, 1, 3, 2], 3, 2, 2]
 VALUE_TWO = [6, [1, 1, 1, 1, 1, 1], 3, 2, 0]
-VALUE_THREE = [1,[4], 4, 1, 1]
-TEST_COLLECTION = [VALUE_ONE, VALUE_TWO, VALUE_THREE]
+VALUE_THREE = [1, [4], 4, 1, 1]
+TEST_COLLECTION = [VALUE_TWO, VALUE_THREE, VALUE_ONE]
+
 
 class BirthdayChocolateTest(unittest.TestCase):
 
     def setUp(self):
         self.calculator = birthday_chocolate.BirthdayChocolate()
-
-    def test_x(self):
-        for values in TEST_COLLECTION:
-            self.assertEqual(values[4], self.calculator.solve_problem(values[0], values[1], values[2], values[3]))
 
     def test_y(self):
         for values in TEST_COLLECTION:
